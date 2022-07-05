42724923c8ad9ed62776b81bcd96394f95054a68
Corey Duke <68204160+cdukedev@users.noreply.github.com>
2022-07-05 10:14:35 -0400

    added 7-5 challenge

---
+++ 7-5.js
@@ -0,0 +1,27 @@
+// ================== doubleUp ======================
+
+// Question:
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+// "Hello World" => "HHeelloo  WWoorrlldd"
+
+// ==================================================
+
+const str = "Hello World";
+
+const doubleUp = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[i] + str[i];
+  }
+  return result;
+};
+
+console.log(doubleUp(str));
+
+
+//redo doubleUp with map and split
+const doubleUp2 = (str) => {
+    return str.split("").map((char) => char + char).join("");
+
+
+//expected output: "HHeelloo  WWoorrlldd"
