c682ff3da1a64e5aa6a49d90df9d09b0e3230d00
Corey Duke <68204160+cdukedev@users.noreply.github.com>
2022-07-08 13:29:15 -0400

    kata 2, learned about Math.abs to find absolute value

--- katas.js
+++ katas.js
@@ -2,16 +2,36 @@
 
 // Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
-
 // kata1
-const stringToNumber = function (str) {
-  return Number(str);
-};
-console.log(stringToNumber("1234"), 1234);
-console.log(stringToNumber("605"), 605);
-console.log(stringToNumber("1405"), 1405);
-console.log(stringToNumber("-7"), -7);
+// const stringToNumber = function (str) {
+//   return Number(str);
+// };
+// console.log(stringToNumber("1234"), 1234);
+// console.log(stringToNumber("605"), 605);
+// console.log(stringToNumber("1405"), 1405);
+// console.log(stringToNumber("-7"), -7);
 
+//kata2
+//my solution
+function makeNegative(num) {
+  if (num < 0) {
+    return num;
+  } else {
+    return -num;
+  }
+}
 
+//higher voted solutions
 
+// as ternary operator
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
+
+//using Math.abs
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+
+console.log(makeNegative(-42));
 
