bf96a265e08d042a1c3e496582318960d902e31f
corey duke <cdukedev@gmail.com>
2022-09-03 12:42:52 -0400

    reorganized challenge folders

--- brain-station-morning-challenge/8.2.js
+++ brain-station-morning-challenge/8.2.js
@@ -13,23 +13,20 @@
 
 // Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-// I can be placed before V (5) and X (10) to make 4 and 9. 
-// X can be placed before L (50) and C (100) to make 40 and 90. 
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
 
+// let romanNumeral = XIV ; //expected output 14
+// let romanNumeral2 = MDXIV ; //expected output 1514
 
-let romanNumeral = XIV ; //expected output 14
-let romanNumeral2 = MDXIV ; //expected output 1514
-
-
-const convertNumeral = () => {
-    //if numeral length = 2 & romanNumeral[0] === I
-    //if numeral length = 3 & romanNumeral[0] === X
-    //if numeral length = 3 & romanNumeral[0] === X
-    //if numeral length = 3 & romanNumeral[0] === X
-}
-
+// const convertNumeral = () => {
+//     //if numeral length = 2 & romanNumeral[0] === I
+//     //if numeral length = 3 & romanNumeral[0] === X
+//     //if numeral length = 3 & romanNumeral[0] === X
+//     //if numeral length = 3 & romanNumeral[0] === X
+// }
 
 // charecteristics
 // let four =  M;
@@ -37,5 +34,11 @@ const convertNumeral = () => {
 // let two = L, X;
 // let one = V, I
 
-
-
+const number = () => {
+  let count = 1;
+  for (let i = 1; i <= 100; i += 2) {
+    count++;
+  }
+  return count;
+};
+console.log(number());
