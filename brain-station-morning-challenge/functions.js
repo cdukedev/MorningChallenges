7e7b4339b4ca7cf21f386f8500c6e6814d1c5dd1
corey duke <cdukedev@gmail.com>
2023-01-06 04:08:51 -0500

    Wrote the two functions for the challenge.

--- brain-station-morning-challenge/functions.js
+++ brain-station-morning-challenge/functions.js
@@ -1,60 +1,71 @@
 //Array of people, there is no need to change this
-const PERSONDATA = [{
-    name:"Michael",
-    rank:4,
-    age:19,
-    score: 66
-},{
-    name:"Emily",
-    rank:7,
-    age:22,
-    score: 37
-},{
-    name:"Sam",
-    rank:2,
-    age:23,
-    score:80
-},{
-    name:"William",
-    rank:10,
-    age:26,
-    score:11
-},{
-    name:"James",
-    rank:8,
-    age:26,
-    score:28
-},{
-    name:"Mia",
-    rank:5,
-    age:28,
-    score:54
-},{
-    name:"Isabella",
-    rank:1,
-    age:31,
-    score:100
-},{
-    name:"Alex",
-    rank:3,
-    age:34,
-    score:75
-},{
-    name:"Olivia",
-    rank:6,
-    age:36,
-    score:42
-},{
-    name:"Geoff",
-    rank:9,
-    age:41,
-    score:19
-}]
+const PERSONDATA = [
+  {
+    name: "Michael",
+    rank: 4,
+    age: 19,
+    score: 66,
+  },
+  {
+    name: "Emily",
+    rank: 7,
+    age: 22,
+    score: 37,
+  },
+  {
+    name: "Sam",
+    rank: 2,
+    age: 23,
+    score: 80,
+  },
+  {
+    name: "William",
+    rank: 10,
+    age: 26,
+    score: 11,
+  },
+  {
+    name: "James",
+    rank: 8,
+    age: 26,
+    score: 28,
+  },
+  {
+    name: "Mia",
+    rank: 5,
+    age: 28,
+    score: 54,
+  },
+  {
+    name: "Isabella",
+    rank: 1,
+    age: 31,
+    score: 100,
+  },
+  {
+    name: "Alex",
+    rank: 3,
+    age: 34,
+    score: 75,
+  },
+  {
+    name: "Olivia",
+    rank: 6,
+    age: 36,
+    score: 42,
+  },
+  {
+    name: "Geoff",
+    rank: 9,
+    age: 41,
+    score: 19,
+  },
+];
 
-search("James",PERSONDATA)  // Should log that James was found at position #5
-search("Eric",PERSONDATA)   // Should log that Eric was not found.
+search("James", PERSONDATA); // Should log that James was found at position #5
+search("Eric", PERSONDATA); // Should log that Eric was not found.
 
-console.log(filter(PERSONDATA, 50)) // Should display an array of everyone with a score greater than 50
+console.log(filter(PERSONDATA, 50)); // Should display an array of everyone with a score greater than 50
 /* Ex:
 [ { name: 'Alex', rank: 3, age: 34, score: 75 },
   { name: 'Isabella', rank: 1, age: 31, score: 100 },
@@ -66,3 +77,21 @@ console.log(filter(PERSONDATA, 50)) // Should display an array of everyone with 
 /*
     Write your functions below.
 */
+const search = (name, array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].name === name) {
+      console.log(`${name} was found at position #${i + 1}`);
+      return;
+    }
+  }
+  console.log(`${name} was not found.`);
+};
+const filter = (array, score) => {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].score > score) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
